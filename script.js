a130407d2674e271537829e6af9693812fd0c9ae
Martin Clark <webdeveloperplug@gmail.com>
2022-09-08 21:33:45 -0500

    that will be the initials and thats it from the main machine, now i will pull a clone from the repo unto the lasies laptop and work on the responsiveness from there. Completed forms, google database.

---
+++ script.js
@@ -0,0 +1,21 @@
+const url = "https://docs.google.com/forms/u/2/d/e/1FAIpQLSdSUiJJ-NS4hS50FlGmUICnn-2u3AdfAdrfdccpm3O4YyBilg/formResponse";
+const form = document.querySelector('form');
+const submitBtn = document.getElementById("submit");
+const name = document.getElementById("name")
+const email = document.getElementById("email");
+const phoneNumber = document.getElementById("telephone");
+const message = document.getElementById("message");
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    submitBtn.disabled = true;
+    submitBtn.innerHTML = "Submitting...";
+    console.log(form);
+    fetch(url, { mode: 'no-cors', method: 'POST', body: new FormData(form)})
+    .then(response => { 
+        submitBtn.disabled = false;
+        submitBtn.innerHTML = "Submitted";
+        submitBtn.innerHTML = "Submit";
+        form.reset();
+        alert('Thanks for committing to the cause!', response)})
+    .catch(error => alert('Error!', error.message))
+})
