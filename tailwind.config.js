5910024270c16bb32e04d1b836474bb9d13c1424
Martin Clark <webdeveloperplug@gmail.com>
2022-11-13 18:39:17 -0600

    Let us see if this is the correct way to deploy a tailwind css project fam. -Geedo Boubacar Ibn Jalloh

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/**/*.{html,js}',
+    './blog/**/*.{hmtl,js}',
+    './shop/**/*.{html,js}',
+    './home/**/*.{hmtl,js}'
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
